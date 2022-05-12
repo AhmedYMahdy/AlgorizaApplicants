a941974bb7bd7b270beed579f6b7f30ad30a2b44
Ahmed Youssef Mahdy <ahmed.mahdy@arabiansystems.net>
2022-05-12 12:33:09 +0200

    Architecture

--- AlgorizaApplicants/Program.cs
+++ AlgorizaApplicants/Program.cs
@@ -11,12 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//DB
 builder.Services.AddDbContext<AlgorizaContext>(opts =>
     opts.UseSqlServer(builder.Configuration.GetConnectionString("AlgorizaCS")));
 builder.Services.AddScoped(typeof(AlgorizaContext));
 
-var swaggerXmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-var swaggerXmlFilePath = Path.Combine(AppContext.BaseDirectory, swaggerXmlFileName);
+//Swagger
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
@@ -26,29 +26,6 @@ builder.Services.AddSwaggerGen(options =>
         Description = "Algoriza.Applicant API Swagger surface"
     });
     options.OperationFilter<AddRequiredHeaderParameter>();
-    //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    //{
-    //    In = ParameterLocation.Header,
-    //    Type = SecuritySchemeType.Http,
-    //    Name = "Authorization",
-    //    Scheme = "bearer",
-    //    BearerFormat = "JWT",
-    //    Description = "Specify the authorization token."
-    //});
-    //options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    //{
-    //    {
-    //        new OpenApiSecurityScheme
-    //        {
-    //            Reference = new OpenApiReference
-    //            {
-    //                Type = ReferenceType.SecurityScheme,
-    //                Id = "Bearer"
-    //            }
-    //        },
-    //        new string[] { }
-    //    }
-    //});
 });
 
 
@@ -69,6 +46,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Algoriza APIs");
+});
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
